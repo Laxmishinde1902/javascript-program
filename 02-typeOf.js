f5c3dcb12f2f9993c6c30f99e42b37d1ddad8a80
Laxmishinde1902 <laxmishinde1902@gmail.com>
2024-07-31 20:16:22 +0530

    Created Bank name and Total Count
    Created Bank name and Total Count

---
+++ 02-typeOf.js
@@ -0,0 +1,34 @@
+console.log("=============================Bank Name==============================");
+var bankname;
+console.log( bankname);
+var banknameTypeOf = typeof bankname;
+console.log("Type of bankName :", banknameTypeOf);
+
+var bankname = 'SBI Bank';
+console.log("My Bank name is : SBI Bank");
+var banknameTypeOf = typeof bankname;
+console.log("Data Type of Bank name :",banknameTypeOf);
+
+console.log("================================Marks================================");
+var marks = '90%';
+console.log("Marks is:", marks);
+var marksTypeOf = typeof marks;
+console.log("Data type of Marks :", marksTypeOf);
+
+console.log("===========================Working================================");
+var isWorking = true;
+console.log("Is Working:" , isWorking);
+var isWorkingTypeOf = typeof isWorking;
+console.log("Data type of isWorking:",isWorkingTypeOf);
+
+console.log("==========================Total Count============================");
+var totalcount = 'Hundred and Seven';
+console.log("Total Count is:" , totalcount);
+var totalcountTypeOf = typeof totalcount;
+console.log("Data Type of Total Count:",totalcountTypeOf);
+
+var totalCount = 107;
+console.log("Total Count is:" , totalCount);
+var totalCountTypeOf = typeof totalCount;
+console.log("Data type of Total Count:" ,totalCountTypeOf);
+
