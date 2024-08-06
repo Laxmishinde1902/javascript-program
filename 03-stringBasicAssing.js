ab97dcc2cd6122981111feff21af685525b4bd6a
Laxmishinde1902 <laxmishinde1902@gmail.com>
2024-08-06 14:04:34 +0530

    created the sum of total number of characters and my hobbies length
    created the sum of total number of characters and my hobbies length

---
+++ 03-stringBasicAssing.js
@@ -0,0 +1,19 @@
+console.log("==========Function with No Argument and No Return value==========");
+function stringBasics(){
+console.log("My Dream Company Name is : TCS");
+}
+stringBasics();
+
+console.log("==============2nd Condition================");
+function myHobbies(hobby1,hobby2,hobby3){
+    var result = hobby1+hobby2+hobby3;
+    console.log("My Hobbies are :",result);
+}
+myHobbies("Treking ","Listening Music " ,"Singing");
+
+console.log("=============Sum of total number of characters============");
+var myHobbies ="Treking, Listening music, Singing";
+
+var myHobbiesLength = myHobbies.length
+console.log("Sum of total number of characters:",myHobbiesLength);
+
