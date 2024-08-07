01ce446cfc7a46115dd1c05c56a5ebaad5d80234
Laxmishinde1902 <laxmishinde1902@gmail.com>
2024-08-07 19:17:43 +0530

    02-typeof : variable , datatype and type of operator created
    02-typeof : variable , datatype and type of operator created

---
+++ 02-typeOfAssing.js
@@ -0,0 +1,49 @@
+console.log("======================1st Condition======================");
+
+var bankName;
+console.log(bankName);
+
+var bankName ="SBI Bank";
+console.log("My Bank name is:",bankName);
+
+var bankNameTypeOf = typeof bankName;
+console.log("Datatype of Bank Name is:",bankNameTypeOf);
+
+console.log("========================Marks============================");
+var marks = '90%';
+console.log("marks is:",marks);
+
+var marksTypeOf = typeof marks;
+console.log("Datatype of marks is:",marksTypeOf);
+
+console.log("======================Is working===========================");
+var isWorking = true;
+console.log("Is working:",isWorking);
+
+var isWorkingTypeOf = typeof isWorking;
+console.log("Datatype of Is Working:",isWorkingTypeOf);
+
+console.log("=====================Total Count==========================");
+var totalCount = "Hundred and Seven";
+console.log("Total Count is:",totalCount);
+
+var totalCountTypeOf = typeof totalCount;
+console.log("Dattaype of Total Count is:",totalCountTypeOf);
+
+var totalCount = 107;
+console.log("Total Count is:",totalCount);
+
+var typeOfTotalCount = typeof totalCount;
+console.log("Datatype of total Count is:",typeOfTotalCount);
+
+
+
+
+
+
+
+
+
+
+
+
