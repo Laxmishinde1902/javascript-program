fd972bb562a72dc969b79f98a521f934d170ecd7
Laxmishinde1902 <laxmishinde1902@gmail.com>
2024-08-12 00:37:36 +0530

    06-for-loop Assingement : Printed the value using for and loop
    06-for-loop Assingement : Printed the value using for and loop

---
+++ 06-for-loopAssing.js
@@ -0,0 +1,40 @@
+console.log(`==================Print numbers from 5 to 15 by incrementing 1================`);
+     for (let i = 5; i <= 15; i++) {
+     console.log(i);    
+    }
+
+console.log(`================Print numbers from 50 to 40 by decrementing by 1==================`);
+     for (let index = 50; index >= 40; index--) {
+     console.log(index);  
+     }
+
+console.log(`=========================First 15 odd numbers==========================`);
+     for (let i = 1; i <= 30; i = i+2) {
+        console.log(i);
+     }
+
+console.log(`========================First 10 even numbers=================================`);
+     for (let i = 2; i <= 20; i = i+2) {
+        console.log(i); 
+     }
+
+console.log(`========================Print table of 5=================================`);
+     for (let i = 5; i <= 50; i = i+5) {
+        console.log(i);
+     }
+
+console.log(`========================Print table of 10====================================`);
+     for (let i = 10; i <=100; i = i+10) {
+        console.log(i);     
+     }
+
+console.log(`=======================Print the table of 10 in reverse order==================`);
+     for (let i = 100; i >=10 ; i = i-10) {
+        console.log(i);
+        
+     }
+
+
+
+
+
