0aa118012870b0fb540fa2119c311a6ccfe4231f
Laxmishinde1902 <laxmishinde1902@gmail.com>
2024-08-14 20:30:32 +0530

    08-arrayAssingement of fruit list
    08-arrayAssingement of fruit list

--- 08-arrayAssing01.js
+++ 08-arrayAssing01.js
@@ -1,84 +1,77 @@
-let fruitList = ["Banana"," Orange", " Apple", " Mango", " Watermelon"]
-   console.log(`Given Fruit list in Array is: ${fruitList}`);
+let fruits = ["Banana","Orange","Apple","Mango","Water melon"];
+console.log(fruits);
+console.log(`The given list of fruits: ${fruits}`);
+
+console.log(`===========Total number of elements in string is=============`);
+    let fruitsLength = fruits.length;
+    console.log(`The total number of elements in the list is :${fruitsLength}`);
+
+console.log(`==========The first name and last name of fruits in list==========`);
+
+     console.log(`The First name of fruit : ${fruits[1]}`);
+
+     console.log(`The last name of fruit : ${fruits[fruits.length-1]}`);
 
 
-console.log(`========Total Number of elements in fruit list=============`);
-      let fruitListLength = fruitList.length;
-      console.log(`The Total number of elements fruit list is: ${fruitListLength} `);
+console.log(`============Add element papaya before banana===================`);
 
+      fruits.unshift("Papaya");
+      console.log(fruits);
 
-console.log(`===============First and last name of fruits===============`);
-    
-      console.log(`The First name of fruit is :${fruitList[1]}`);
-
-      console.log(`The last name of fruit is : ${fruitList[fruitList.length-1]}`);
-
-
-console.log(`=========Replace an element orange with kiwi============`);
-       console.log(fruitList);
-       fruitList[1] = " Kiwi"
-       console.log(fruitList);
-
-
-
-console.log(`===============Insert an element 'pineapple' at the last position=========`);
-        fruitList.push("Pineapple");
-        console.log(fruitList);
-
-
-console.log(`============Add element papaya before Banana Element=================`);
-         fruitList.unshift("Papaya");
-         console.log(fruitList);
-
-
-console.log(`=============Log the elements from 1 to 4==========================`);
-        let fruitListSlice = fruitList.slice(1,4);
-        console.log(fruitListSlice);
-
-
-
-console.log(`=================Remove mango from array=============================`);
-       console.log(fruitList);
+console.log(`===========Remove Mango from array==============================`);
+       console.log(fruits);
        
-       let fruitListSplice31 = fruitList.splice(4,1);
+       let splice41 = fruits.splice(4,1);
+       console.log(fruits);
+       console.log(splice41);
+       
+
+console.log(`============Add element Pineapple at the last position============`);
+
+      fruits.push("Pineapple");
+      console.log(fruits);
+
+console.log(`===========Insert an element "Dragon Fruit" before "Water melon"`);
+      
+      console.log(fruits);
+       let fruitsSplice = fruits.splice(5,0 ,"DragonFruit");
+       console.log(fruits);
+       console.log(fruitsSplice);
+       
+
+ console.log(`=============Replace an element "orange" with "Kiwi" `);
+      
+      console.log(fruits);
+       fruits[2] = "Kiwi";
+       console.log(fruits);
+
+console.log(`==============log elements starting from index 1 to 4==========`);
+      
+      console.log(fruits);
+      let fruitsSlice14 = fruits.slice(1,4);
+      console.log(fruitsSlice14);
+
+console.log(`=============Select last 3 elements and log on console=============`);
+       
+       let fruitList = fruits[fruits.length-3];
        console.log(fruitList);
-       console.log(fruitListSplice31);
-
-
-console.log(`=============Insert element DragonFruit before Water melon=================`);
-      console.log(fruitList);
-
-      let fruitListSplice = fruitList.splice(4,0,"DragonFruit");
-      console.log(fruitList);
+       let fruitList2 = fruits[fruits.length-2];
+       console.log(fruitList2);
+       let fruitList3 = fruits[fruits.length-1];
+       console.log(fruitList3);
+       
+       
+       
+       
+       
       
       
 
        
-
-
-       
-       
+      
        
 
-        
-
-         
-
-        
-
-       
-
-       
-    
+      
      
 
-    
-    
-    
-    
-    
-   
 
-   
-   
-   
